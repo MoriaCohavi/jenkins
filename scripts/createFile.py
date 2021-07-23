554536f0c3f5dddf0ea353d22446f5e1fb2802da
cohavimoria <cohavimoria@gmail.com>
2021-07-23 11:52:45 +0300

    createFile script fix

--- scripts/createFile.py
+++ scripts/createFile.py
@@ -1,4 +1,14 @@
-rm -r ~/DevOps
-mkdir ~/DevOps
-touch ~/DevOps/DevOps.txt
-echo "Hello World" > ~/DevOps/DevOps.txt
+# Opening a file
+file1 = open('myfile.txt', 'w')
+L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+s = "Hello\n"
+  
+# Writing a string to file
+file1.write(s)
+  
+# Writing multiple strings
+# at a time
+file1.writelines(L)
+  
+# Closing file
+file1.close()
